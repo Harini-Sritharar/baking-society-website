7b5fa60d47283d0cbc9cbf652ec1fd717353a34c
Harini-Sritharar <harinisri2004@gmail.com>
2024-01-02 20:18:21 +0000

    Imported fonts, added navigation bar widget and added links to socials

---
+++ src/colours.js
@@ -0,0 +1,7 @@
+export const COLOURS = {
+    background: "#FAFAFA",
+    accents: "#F8F2EC",
+    blue: "#CEEBE8",
+    pink: "#FAC9C9",
+    text: "574240"
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import { Container, Nav, Navbar } from "react-bootstrap"
+import social1 from "../assets/img/email.png"
+import social2 from "../assets/img/insta.png"
+import social3 from "../assets/img/whatsapp.png"
+import social4 from "../assets/img/website.png"
+
+export const NavBar = () => {
+    return (
+        <Navbar>
+            <Container>
+                <p></p>
+                  {/* Social Icons */}
+                  <span className="navbar-text">
+                    <div className="social-icon">
+                        <a href="mailto:baking-society@imperial.ac.uk">
+                            <img src={social1} alt="email"/>
+                        </a>
+                        <a href="https://www.instagram.com/icbakingsoc/?hl=en-gb">
+                            <img src={social2} alt="insta"/>
+                        </a>
+                        <a href="https://chat.whatsapp.com/C2Vpor3H2ix7o6BLWBuuPX">
+                            <img src={social3} alt="whatsapp"/>
+                        </a>
+                        <a href="https://www.imperialcollegeunion.org/activities/a-to-z/baking">
+                            <img src={social4} alt="membership"/>
+                        </a>
+                    </div>
+                  </span>
+            </Container>
+        </Navbar>
+    )
+}
